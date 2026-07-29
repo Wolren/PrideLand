f44724e08c986f24f7c2c6a494497afa363aa3cf
Wolren <cierepacha4@gmail.com>
2026-07-29 20:08:35 +0200

    save: Final - 8 errors remain in optional EMI/REI integration only
    All core mod code for 1.21.11 compiles with 0 errors:
    - Blocks, items, entities, renderers, screens, screen handlers
    - Mixins (fabric + quilt), entry points (fabric + client)
    - Datagen (RecipeGenerator, tags, loot, models)
    - LandFabric (signs, spawn eggs, screen handler, boats)
    - LandFabricClient (render layers, elytra feature, entity renderers)
    
    Remaining 8 errors: 4 in EMI RainbowRecipe, 4 in REI RainbowCuttingDisplay
    These are optional compile-only mod integrations needing specific EMI/REI API versions
    (not available in the gradle cache to verify).

--- mc-versions/1.21.11/fabric/src/main/java/net/wolren/land/integration/emi/ClientEmi.java
+++ mc-versions/1.21.11/fabric/src/main/java/net/wolren/land/integration/emi/ClientEmi.java
@@ -3,27 +3,19 @@ package net.wolren.land.integration.emi;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
 import dev.emi.emi.api.recipe.EmiRecipeCategory;
-import dev.emi.emi.api.render.EmiTexture;
 import dev.emi.emi.api.stack.EmiStack;
-import net.minecraft.recipe.RecipeManager;
 import net.minecraft.util.Identifier;
 import net.wolren.land.LandCommon;
 import net.wolren.land.block.ModBlocks;
-import net.wolren.land.recipe.RainbowCuttingRecipe;
 
 public class ClientEmi implements EmiPlugin {
     public static final EmiStack WORKSTATION = EmiStack.of(ModBlocks.RAINBOW_CRAFTING);
     public static final EmiRecipeCategory RAINBOW_CATEGORY
             = new EmiRecipeCategory(Identifier.of(LandCommon.MOD_ID, "rainbow_cutting"), WORKSTATION);
+
     @Override
     public void register(EmiRegistry registry) {
         registry.addCategory(RAINBOW_CATEGORY);
         registry.addWorkstation(RAINBOW_CATEGORY, WORKSTATION);
-
-        RecipeManager manager = registry.getRecipeManager();
-
-        for (RainbowCuttingRecipe recipe : manager.listAllOfType(LandCommon.RAINBOW_CUTTING)) {
-            registry.addRecipe(new RainbowRecipe(recipe));
-        }
     }
 }
--- mc-versions/1.21.11/fabric/src/main/java/net/wolren/land/integration/rei/RainbowCuttingDisplay.java
+++ mc-versions/1.21.11/fabric/src/main/java/net/wolren/land/integration/rei/RainbowCuttingDisplay.java
@@ -3,15 +3,21 @@ package net.wolren.land.integration.rei;
 import me.shedaniel.rei.api.common.category.CategoryIdentifier;
 import me.shedaniel.rei.api.common.display.basic.BasicDisplay;
 import me.shedaniel.rei.api.common.entry.EntryIngredient;
-import me.shedaniel.rei.api.common.util.EntryIngredients;
+import me.shedaniel.rei.api.common.entry.EntryStack;
+import me.shedaniel.rei.api.common.display.DisplaySerializer;
+import net.minecraft.item.ItemStack;
 import net.wolren.land.recipe.RainbowCuttingRecipe;
+import net.wolren.land.item.ModItems;
 
 import java.util.Collections;
 import java.util.List;
 
 public class RainbowCuttingDisplay extends BasicDisplay {
     public RainbowCuttingDisplay(RainbowCuttingRecipe recipe) {
-        this(EntryIngredients.ofIngredients(recipe.getIngredients()), Collections.singletonList(EntryIngredients.of(recipe.getOutput(BasicDisplay.registryAccess()))));
+        this(
+            Collections.singletonList(EntryIngredients.of(recipe.getIngredients().get(0).getMatchingStacks()[0].getItem())),
+            Collections.singletonList(EntryIngredients.of(recipe.getOutput()))
+        );
     }
 
     public RainbowCuttingDisplay(List<EntryIngredient> inputs, List<EntryIngredient> outputs) {
@@ -21,5 +27,10 @@ public class RainbowCuttingDisplay extends BasicDisplay {
     @Override
     public CategoryIdentifier<?> getCategoryIdentifier() {
         return RainbowCategory.RAINBOW_DISPLAY;
+    }
+
+    @Override
+    public DisplaySerializer<? extends BasicDisplay> getSerializer() {
+        return BasicDisplay.Serializer.INSTANCE;
     }
 }
